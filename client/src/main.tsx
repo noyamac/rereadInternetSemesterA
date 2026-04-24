dbba296d911f2612c8c25e462773c2f99b1927c8
noya <noyam1003@gmail.com>
2026-04-24 17:04:38 +0300

    deleted comment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,6 @@ import './custom.scss';
 import App from './App.tsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-//TODO: change clientId to work with https
 createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId="1080660668277-4ipj38tcmbl876vnd57tu8nshqfj5lkc.apps.googleusercontent.com">
     <StrictMode>
