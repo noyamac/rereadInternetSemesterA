86b6ccdcee0990904f4b7a9e582b4b892e05bd3d
yarin <yarin12501@gmail.com>
2026-04-25 13:03:32 +0300

    fix

--- server/src/tests/file.test.ts
+++ server/src/tests/file.test.ts
@@ -22,9 +22,7 @@ describe('File Tests', () => {
         .attach('file', filePath);
       expect(response.statusCode).toEqual(200);
       let url = response.body.url;
-      console.log('Uploaded 1 file URL: ' + url);
       url = url.replace(/^.*\/\/[^/]+/, '');
-      console.log('Uploaded 2 file URL: ' + url);
       const res = await request(app).get(url);
       expect(res.statusCode).toEqual(200);
     } catch (err) {
